007ff1147fcc39f9baa2be6a30d97ac397140c36
Jheysson Urbano <57603867+Jheysson@users.noreply.github.com>
2022-10-15 21:36:00 -0500

    Video 346

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,21 +1,10 @@
 import React from 'react'
 import { Navbar } from '../'
-import { Calendar, dateFnsLocalizer} from 'react-big-calendar'
-import enUS from 'date-fns/locale/en-US'
+import { Calendar} from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
-import { addHours, parse, format, startOfWeek, getDay } from 'date-fns'
-
-const locales = {
-  'en-US': enUS,
-}
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-})
+import { addHours } from 'date-fns'
+import { getMessgesES, localizer } from '../../helpers'
 
 
 const events = [{
@@ -26,16 +15,35 @@ const events = [{
 }]
 
 export const CalendarPage = () => {
+
+  const eventStyleGetter = (event, start, end, isSelected ) => {
+    console.log({event, start, end, isSelected})
+
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return {
+      style
+    }
+  }
+
   return (
     <>
       <Navbar />
       <Calendar
-      localizer={localizer}
-      events={events}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: 'calc(100vh - 80px)' }}
-    />
+        culture='es'
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+        messages = {getMessgesES()}
+        eventPropGetter={eventStyleGetter}
+      />
     </>
   )
 }
---
+++ src/helpers/calendarLocalizer.js
@@ -0,0 +1,14 @@
+import { parse, format, startOfWeek, getDay } from 'date-fns'
+import { dateFnsLocalizer} from 'react-big-calendar'
+import esES from 'date-fns/locale/es'
+
+const locales = {
+  'es': esES,
+}
+export const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
