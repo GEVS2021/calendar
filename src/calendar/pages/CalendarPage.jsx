5e41c8f8bf3d580ac8a2c98b0e41c4bded0e0d38
Jheysson Urbano <57603867+Jheysson@users.noreply.github.com>
2022-10-15 21:23:40 -0500

    Video 345

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import { Navbar } from '../'
+import { Calendar, dateFnsLocalizer} from 'react-big-calendar'
+import enUS from 'date-fns/locale/en-US'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+import { addHours, parse, format, startOfWeek, getDay } from 'date-fns'
+
+const locales = {
+  'en-US': enUS,
+}
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+
+const events = [{
+  title: 'Cumpleaños',
+  notes:'Example note',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+}]
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
+      <Calendar
+      localizer={localizer}
+      events={events}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 'calc(100vh - 80px)' }}
+    />
     </>
   )
 }
