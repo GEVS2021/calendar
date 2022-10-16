122adb0360df846d554c8156650fc9978d88ce21
EduEstula1003 <edu.estula@ext.blaze.me>
2022-10-16 12:49:28 -0500

    345. React Big Calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import { addHours, format, parse, startOfWeek, getDay } from "date-fns";
+import enUS from 'date-fns/locale/en-US'
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
 import { Navbar } from '../'
+
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [{
+  title: "Cumpleaños del Jefe",
+  notes: "Hay que comprar el regalo",
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: "fafafa",
+  user: {
+    _id: "123",
+    name: "Fernando"
+  }
+}];
 
 export const CalendarPage = () => {
   return (
     <>
-      {/* <div>CalendarPage</div> */}
       <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 80px)" }}
+      />
     </>
   )
 }
