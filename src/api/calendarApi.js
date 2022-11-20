c153d742df08b6f986130d6e66a2f8b6d1f72b45
EduEstula1003 <edu.estula@ext.blaze.me>
2022-11-20 17:39:53 -0500

    408. Axios - Configurar cliente para peticiones HTTP

---
+++ src/api/calendarApi.js
@@ -0,0 +1,13 @@
+import { getEnvVariables } from "../calendar/helpers/getEnvVariables";
+import axios from "axios";
+
+
+const { VITE_API_URL } = getEnvVariables();
+
+const calendarApi = axios.create({
+    baseURL: VITE_API_URL
+})
+
+// Todo: configurar interceptores
+
+export default calendarApi;
